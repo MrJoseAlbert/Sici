21329e9299404d03faf2b9cfd75ede3c2e32dd3a
MrJoseAlbert <jose.a2510@hotmail.com>
2024-04-23 10:22:42 -0600

    Se creo una nueva ruta

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
 Route::group(['prefix'=> 'maestro'], function () {
     Route::get('/m',[MaestroController::class, 'get']);
 });
